59872a0e9ff8c860e6537df4c27eacc04dadfe2b
nakatanikodai <nakatanixx1123@gmail.com>
2020-05-06 04:12:28 +0900

    ｄ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -26,7 +26,7 @@ class LinebotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
-          if event.message['text'].eql?('川島永嗣')
+          if event.message['text'].eql?('川島')
             # private内のtemplateメソッドを呼び出します。
             client.reply_message(event['replyToken'], kawashima)
           end
