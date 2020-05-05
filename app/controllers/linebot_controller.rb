817a8ac4ce873374899fe2059b66f36077e42e22
nakatanikodai <nakatanixx1123@gmail.com>
2020-05-05 22:42:40 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -39,43 +39,46 @@ class LinebotController < ApplicationController
 
   private
 
-
-  {
-    "type": "template",
-    "altText": "This is a buttons template",
-    "template": {
-        "type": "buttons",
-        "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-        "imageAspectRatio": "rectangle",
-        "imageSize": "cover",
-        "imageBackgroundColor": "#FFFFFF",
-        "title": "Menu",
-        "text": "Please select",
-        "defaultAction": {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-        },
-        "actions": [
-            {
-              "type": "postback",
-              "label": "Buy",
-              "data": "action=buy&itemid=123"
-            },
-            {
-              "type": "postback",
-              "label": "Add to cart",
-              "data": "action=add&itemid=123"
-            },
-            {
+  def template
+    {
+      "type": "template",
+      "altText": "This is a buttons template",
+      "template": {
+          "type": "buttons",
+          "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+          "imageAspectRatio": "rectangle",
+          "imageSize": "cover",
+          "imageBackgroundColor": "#FFFFFF",
+          "title": "Menu",
+          "text": "Please select",
+          "defaultAction": {
               "type": "uri",
               "label": "View detail",
               "uri": "http://example.com/page/123"
-            }
-        ]
+          },
+          "actions": [
+              {
+                "type": "postback",
+                "label": "Buy",
+                "data": "action=buy&itemid=123"
+              },
+              {
+                "type": "postback",
+                "label": "Add to cart",
+                "data": "action=add&itemid=123"
+              },
+              {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+              }
+          ]
+      }
     }
-  }
-  #def template
+  end
+end
+
+#def template
   #  {
   #    "type": "template",
   #    "altText": "this is a confirm template",
@@ -99,4 +102,3 @@ class LinebotController < ApplicationController
   #    }
   #  }
   #end
-end
