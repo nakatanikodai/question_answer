ea0fe05b3b0fcabf00b62a9dbf9f125c6d151ad1
nakatanikodai <nakatanixx1123@gmail.com>
2020-05-05 10:01:01 +0900

    linebot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -4,9 +4,9 @@ class LinebotController < ApplicationController
   protect_from_forgery :except => [:callback]
 
   def client
-    @client ||= Line::Bot::Client.new{ |config|
+    @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_secret = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 
@@ -14,18 +14,20 @@ class LinebotController < ApplicationController
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, singature)
+    unless client.validate_signature(body, signature)
       head :bad_request
     end
 
     events = client.parse_events_from(body)
 
-    events.each{ |event|
+    events.each { |event|
       case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::Message
+        when Line::Bot::Event::MessageType::Text
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
           if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
             client.reply_message(event['replyToken'], template)
           end
         end
