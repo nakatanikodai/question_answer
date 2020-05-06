a1c8c70b5cb4d16c897ebe8a1c1ed1ede454ff7f
nakatanikodai <nakatanixx1123@gmail.com>
2020-05-06 17:01:58 +0900

    bot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -41,35 +41,44 @@ class LinebotController < ApplicationController
 
   def template
     {
-      "type": "carousel",
-      "contents": [
-        {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "First bubble"
-              }
-            ]
+      "type": "bubble",
+      "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Header text"
           }
-        },
-        {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Second bubble"
-              }
-            ]
+        ]
+      },
+      "hero": {
+        "type": "image",
+        "url": "https://example.com/flex/images/image.jpg"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Body text"
           }
-        }
-      ]
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Footer text"
+          }
+        ]
+      },
+      "styles": {
+        "comment": "See the example of a bubble style object"
+      }
     }
   end
 end
