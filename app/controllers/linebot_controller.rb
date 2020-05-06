4a1bbf7514708525bb2970acf7327c7c59cee6be
nakatanikodai <nakatanixx1123@gmail.com>
2020-05-06 18:40:20 +0900

    bot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -44,8 +44,22 @@ class LinebotController < ApplicationController
       "type": "template",
       "altText": "this is a confirm template",
       "template": {
-          "type": "text",
+          "type": "confirm",
           "text": "今日のもくもく会は楽しいですか？",
+          "actions": [
+              {
+                "type": "message",
+                # Botから送られてきたメッセージに表示される文字列です。
+                "label": "楽しい",
+                # ボタンを押した時にBotに送られる文字列です。
+                "text": "楽しい"
+              },
+              {
+                "type": "message",
+                "label": "楽しくない",
+                "text": "楽しくない"
+              }
+          ]
       }
     }
   end
