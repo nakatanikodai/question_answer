9eaa5524ba1a2abb2e654ab580ad56750c1c151b
nakatanikodai <nakatanixx1123@gmail.com>
2020-05-07 21:53:32 +0900

    bot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,97 +1,65 @@
 class LinebotController < ApplicationController
-  require 'sinatra'
-require 'line/bot'
+  require 'line/bot'
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_id = ENV["LINE_CHANNEL_ID"]
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
+  protect_from_forgery :except => [:callback]
 
-post '/callback' do
-  body = request.body.read
-
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 
-  events = client.parse_events_from(body)
-  events.each do |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
-      end
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head :bad_request
     end
-  end
 
-  # Don't forget to return a successful response
-  "OK"
-end
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
+          if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
+            client.reply_message(event['replyToken'], template)
+          end
+        end
+      end
+    }
+
+      head :ok
+    end
 
   private
 
   def template
     {
-      "type": "bubble",
-      "styles": {
-        "header": {
-          "backgroundColor": "#ffaaaa"
-        },
-        "body": {
-          "backgroundColor": "#aaffaa"
-        },
-        "footer": {
-          "backgroundColor": "#aaaaff"
-        }
-      },
-      "header": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "header"
-          }
-        ]
-      },
-      "hero": {
-        "type": "image",
-        "url": "https://example.com/flex/images/image.jpg",
-        "size": "full",
-        "aspectRatio": "2:1"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "body"
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "footer"
-          }
-        ]
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": "カレーライスはお好きですか？",
+          "actions": [
+              {
+                "type": "message",
+                # Botから送られてきたメッセージに表示される文字列です。
+                "label": "好き",
+                # ボタンを押した時にBotに送られる文字列です。
+                "text": "好き"
+              },
+              {
+                "type": "message",
+                "label": "嫌い",
+                "text": "嫌い"
+              }
+          ]
       }
     }
   end
